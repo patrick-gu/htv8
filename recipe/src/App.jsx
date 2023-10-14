f3eb1f6456d9745485d50038437ab2e85fde6893
Andre Ke <84885039+nohyp3@users.noreply.github.com>
2023-10-14 15:58:33 -0400

    NEW MASTER

--- recipe/src/App.jsx
+++ recipe/src/App.jsx
@@ -399,17 +399,17 @@ function Ingredients({
       });
     // console.log("bump screen")
     setScreenId(1);
-    fetch("http://127.0.0.1:8080/recipe/suggest", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ingredients: ingredientsList }),
-    }).then(async (response) => {
-      const { content } = await response.json();
-      console.log(content);
-      setRecipe(content);
-    });
+    // fetch("http://127.0.0.1:8080/recipe/suggest", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ ingredients: ingredientsList }),
+    // }).then(async (response) => {
+    //   const { content } = await response.json();
+    //   console.log(content);
+    //   setRecipe(content);
+    // });
   };
 
   //append ingredient to list
