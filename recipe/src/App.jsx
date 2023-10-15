f97f1e5cfb24bd7861480f8af484c837ab50b09a
maaiv <jj20050919@gmail.com>
2023-10-15 04:42:38 -0400

    Worked on initial rendering

--- recipe/src/App.jsx
+++ recipe/src/App.jsx
@@ -566,6 +566,9 @@ function UpdateMapRoute(storesSelected, postalCode) {
           console.log(response);
           console.log(time);
           // time = total trip time
+          let timeElement = document.getElementsByClassName("text-2xl font-bold text-center mb-3")[0];
+          timeElement.textContent = `${time} minutes of travel time! 🥳`;
+
       })
 
     }
@@ -637,6 +640,7 @@ async function getMinDistNoDisplay(start, places, waypoints, time) {
 
 
 
+
 function StoreSuggestions({ selectedStores, setSelectedStores, suggestions }) {
   // For now, let's assume it's a simple array of suggestions
 //   const suggestions = [
@@ -648,7 +652,7 @@ function StoreSuggestions({ selectedStores, setSelectedStores, suggestions }) {
 
   return (
     <div>
-    <h2 className="text-2xl font-bold">Store Suggestions:</h2>
+    <h2 className="text-2xl font-bold text-center mb-3">Store Suggestions:</h2>
     <div
       className={`suggestions ${
         selectedStores.length > 0 ? "block" : "hidden "
@@ -679,13 +683,7 @@ function App() {
   const [ingredientsList, setIngredientsList] = useState([]); // State to store the list of ingredients
   const [quantities, setQuantity] = useState([]);
   const [storesSelected, setStoresSelected] = useState([
-    "No Frills",
-    "Walmart",
-    "FreshCo",
-    "Real Canadian Superstore",
-    "Metro",
-    "Food Basics",
-    "Costco",
+
   ]);
   const [currentCost, setCurrentCost] = useState(0);
   const [currentItems, setCurrentItems] = useState([]);
@@ -789,6 +787,7 @@ function Ingredients({
         console.log(response.data[0]);
         setCurrentCost(response.data[1].cost);
         MapRoute(() => findSuggestions({ postalCode: "", storesSelected, ingredientsList, quantities }).then((s) => setSuggestions(s)));
+
       })
       .catch(function (error) {
         console.log(error);
@@ -867,7 +866,7 @@ function Ingredients({
           </h1>
           </div>
           <p className="text-center text-2xl">
-            Start by adding the ingredients you want to buy
+            Start by adding the items you want to buy 😎
           </p>
           <div className="flex justify-center items-center">
             <div className=" font-custom block w-fit p-6 bg-whitesmoke rounded-lg drop-shadow dark:border-gray-700 justify-center items-center hover:animate-bounce">
@@ -889,14 +888,14 @@ function Ingredients({
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <h2 className="text-3xl font-bold text-center my-1.5">Grocery Items</h2>
+          <h2 className="text-3xl font-bold text-center my-1.5">Grocery Items 😋</h2>
           <div className="mb-4">
             <div className="flex items-center">
               <input
                 className="shadow appearance-none border rounded w-fit py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-4"
                 id="ingredient"
                 type="text"
-                placeholder="Enter your ingredient!"
+                placeholder="Enter your items!"
                 value={ingredient}
                 onChange={handleIngredientChange}
                 onKeyUp={handleEnter}
@@ -922,12 +921,12 @@ function Ingredients({
                       </div>
                       <div className="flex items-center space-x-3 ml-4">
                       <div className="caret" onClick={()=>{handleQuantityUp(index)}}>
-                        <svg class="w-6 h-6 text-black dark:text-white hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                        <svg class="w-6 h-6 text-black hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"/>
                         </svg>
                       </div>
                       <div className="caret" onClick={()=>{handleQuantityDown(index)}}>
-                        <svg class="w-6 h-6 text-black dark:text-white hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8" >
+                        <svg class="w-6 h-6 text-black hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8" >
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
                         </svg>
                       </div>
@@ -1142,7 +1141,7 @@ function Another({
       </div>
       <div className="space-y-8 flex-grow">
         <h1 className="text-4xl font-bold text-raisin-black">
-          Let{"'"}s figure out your grocery run!
+          Let{"'"}s figure out your grocery run! 🤩
         </h1>
         {/* Postal Code input field */}
         <div className="flex items-center">
@@ -1215,8 +1214,8 @@ export function SuggestRecipes({ setScreenId, recipe }) {
         </button> */}
       </div>
       <div className="space-y-8 flex-grow">
-        <h1 className="text-4xl font-bold">Need help with meal prep?</h1>
-        <h2 className="text-xl">Here&apos;s a recipe suggestion:</h2>
+        <h1 className="text-4xl font-bold">Need help with meal prep? 🤔</h1>
+        <h2 className="text-xl">Here&apos;s a recipe suggestion! 😍</h2>
         <p className="whitespace-pre-line">
           {recipe.length !== 0 ? recipe : "Loading a recipe..."}
         </p>
@@ -1284,7 +1283,7 @@ export function GetYourStuff({ setScreenId }) {
         }}
       >
       <div className="relative w-1/2 max-h-screen flex flex-col space-y-4 p-4 left-1/4 rounded-lg items-start bg-white drop-shadow">
-        <h1 className="text-4xl font-bold">Complete Your Payment</h1>
+        <h1 className="text-4xl font-bold">Complete Your Payment 💵</h1>
         {approved ?(<img src={checkMark} />):(
         <div>  
         <label class="block text-raisin-black font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
